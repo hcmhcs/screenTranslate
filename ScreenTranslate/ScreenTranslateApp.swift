c283c091419a6d4cf0a9ccab302a17cac09897d2
Changmin Han <49056953+hcmhcs@users.noreply.github.com>
2026-03-02 14:39:31 +0900

    init: init project

---
+++ ScreenTranslate/ScreenTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTranslateApp.swift
+//  ScreenTranslate
+//
+//  Created by 한창민 on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
